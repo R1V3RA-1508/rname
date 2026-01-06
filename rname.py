14c38b8a3bea13250354a2a32bdc7fb6a25896b8
r1v3ra-1508 <dsd436938@gmail.com>
2026-01-06 21:20:39 +0300

    бля нахуя я добавил SameNameError

--- rname.py
+++ rname.py
@@ -5,30 +5,26 @@ import os
 import sys
 from argparse import ArgumentParser
 
-
-class SameNameError(Exception):
-    pass
-
-
 parser = ArgumentParser(prog="rname", description="rename but better")
 parser.add_argument("oldname", help="Old filename", type=str)
 parser.add_argument("newname", help="New filename", type=str)
 args = parser.parse_args()
 
 if args.oldname == args.newname:
-    raise SameNameError(
+    print(
         f"No, you don't need to rename {args.oldname} to {args.newname}. The file's name is same"
     )
+    sys.exit(0)
 try:
     os.rename(args.oldname, args.newname)
     print(f"Successfully renamed {args.oldname} to {args.newname}")
     sys.exit(0)
 except FileNotFoundError:
-    print(f"{args.oldname} not exists")
+    print(f"ERROR: {args.oldname} not exists")
     sys.exit(1)
 except PermissionError:
-    print(f"Not enough permissions")
+    print("ERROR: Not enough permissions")
     sys.exit(1)
 except Exception:
-    print(f"Unknown error")
+    print("ERROR: Unknown error")
     sys.exit(1)
