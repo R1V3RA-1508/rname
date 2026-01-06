a49497b36a288e6b5ad8004618d5e793ebe480f6
r1v3ra-1508 <dsd436938@gmail.com>
2026-01-06 21:28:02 +0300

    feat: logging

--- rname.py
+++ rname.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import logging
 from argparse import ArgumentParser
 
 parser = ArgumentParser(prog="rname", description="rename but better")
@@ -10,21 +11,30 @@ parser.add_argument("oldname", help="Old filename", type=str)
 parser.add_argument("newname", help="New filename", type=str)
 args = parser.parse_args()
 
-if args.oldname == args.newname:
-    print(
-        f"No, you don't need to rename {args.oldname} to {args.newname}. The file's name is same"
+
+def main():
+    if args.oldname == args.newname:
+        logging.error(
+            f"No, you don't need to rename {args.oldname} to {args.newname}. The file's name is same"
+        )
+        sys.exit(0)
+    try:
+        os.rename(args.oldname, args.newname)
+        logging.info(f"Successfully renamed {args.oldname} to {args.newname}")
+        sys.exit(0)
+    except FileNotFoundError:
+        logging.error(f"{args.oldname} not exists")
+        sys.exit(1)
+    except PermissionError:
+        logging.error("Not enough permissions")
+        sys.exit(1)
+    except Exception as e:
+        logging.error(f"{e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO, stream=sys.stdout, format="%(levelname)s: %(message)s"
     )
-    sys.exit(0)
-try:
-    os.rename(args.oldname, args.newname)
-    print(f"Successfully renamed {args.oldname} to {args.newname}")
-    sys.exit(0)
-except FileNotFoundError:
-    print(f"ERROR: {args.oldname} not exists")
-    sys.exit(1)
-except PermissionError:
-    print("ERROR: Not enough permissions")
-    sys.exit(1)
-except Exception:
-    print("ERROR: Unknown error")
-    sys.exit(1)
+    main()
